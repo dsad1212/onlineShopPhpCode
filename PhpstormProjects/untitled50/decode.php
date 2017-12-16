8cc890530e92a4bbc550e705f8d7ec0ed6297dd1
dsad1212 <daniel5431@gmail.com>
2017-12-16 16:27:33 +0200

    Delete decode.php

--- PhpstormProjects/untitled50/decode.php
+++
@@ -1,5 +0,0 @@
-<?php
-$str='aHR0cHM6LAy93d3cuc2hhYmFrLmvdvi5pbC9jeWJlcnRIY2hub2xvZ3kvUGFnZXMvMDExMTAaMTEtMDExMDEwMDAtMDExMDAwMDEtMDExMDAwMTAtMDExMDAwMDEtMDExMDEwME=tLmFzcHg';
-echo base64_decode($str);
-?>
-
